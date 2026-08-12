13694f17e31b42ca2ceadf055dacfc7d11d86113
Hussain <113285362+HussainMayoof@users.noreply.github.com>
2026-08-12 21:32:34 +0300

    Removed project section temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
